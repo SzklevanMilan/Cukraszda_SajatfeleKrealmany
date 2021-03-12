f00fd66bdec9d8f0c5ee24a34b746f13e16896de
SzklevanMilan <szklevanm@gmail.com>
2021-03-12 08:26:17 +0100

    Add project files.

---
+++ Cukraszda/Form1.Designer.cs
@@ -0,0 +1,296 @@
+﻿namespace Cukraszda
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbMaiAjanlat = new System.Windows.Forms.TextBox();
+            this.tbValaszthato = new System.Windows.Forms.TextBox();
+            this.tbLegdragabbNev = new System.Windows.Forms.TextBox();
+            this.tbLegdragabbAr = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.tbLegolcsobbNev = new System.Windows.Forms.TextBox();
+            this.tbLegolcsobbAr = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.textBox10 = new System.Windows.Forms.TextBox();
+            this.textBox11 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.chkBDij = new System.Windows.Forms.CheckBox();
+            this.btnMentes = new System.Windows.Forms.Button();
+            this.btnFelvetel = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tbMaiAjanlat
+            // 
+            this.tbMaiAjanlat.Location = new System.Drawing.Point(12, 12);
+            this.tbMaiAjanlat.Name = "tbMaiAjanlat";
+            this.tbMaiAjanlat.Size = new System.Drawing.Size(229, 20);
+            this.tbMaiAjanlat.TabIndex = 0;
+            // 
+            // tbValaszthato
+            // 
+            this.tbValaszthato.Location = new System.Drawing.Point(12, 57);
+            this.tbValaszthato.Name = "tbValaszthato";
+            this.tbValaszthato.Size = new System.Drawing.Size(229, 20);
+            this.tbValaszthato.TabIndex = 1;
+            // 
+            // tbLegdragabbNev
+            // 
+            this.tbLegdragabbNev.Location = new System.Drawing.Point(12, 149);
+            this.tbLegdragabbNev.Name = "tbLegdragabbNev";
+            this.tbLegdragabbNev.Size = new System.Drawing.Size(229, 20);
+            this.tbLegdragabbNev.TabIndex = 2;
+            // 
+            // tbLegdragabbAr
+            // 
+            this.tbLegdragabbAr.Location = new System.Drawing.Point(261, 149);
+            this.tbLegdragabbAr.Name = "tbLegdragabbAr";
+            this.tbLegdragabbAr.Size = new System.Drawing.Size(120, 20);
+            this.tbLegdragabbAr.TabIndex = 3;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(261, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(89, 100);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // tbLegolcsobbNev
+            // 
+            this.tbLegolcsobbNev.Location = new System.Drawing.Point(12, 215);
+            this.tbLegolcsobbNev.Name = "tbLegolcsobbNev";
+            this.tbLegolcsobbNev.Size = new System.Drawing.Size(229, 20);
+            this.tbLegolcsobbNev.TabIndex = 5;
+            // 
+            // tbLegolcsobbAr
+            // 
+            this.tbLegolcsobbAr.Location = new System.Drawing.Point(261, 215);
+            this.tbLegolcsobbAr.Name = "tbLegolcsobbAr";
+            this.tbLegolcsobbAr.Size = new System.Drawing.Size(120, 20);
+            this.tbLegolcsobbAr.TabIndex = 6;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(135, 274);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(229, 20);
+            this.textBox7.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 113);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(130, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Legdrágább süteményünk";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(29, 188);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(128, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Legolcsóbb süteményünk";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(29, 274);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Süti típusa";
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(469, 34);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(194, 20);
+            this.textBox8.TabIndex = 11;
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(469, 72);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(194, 20);
+            this.textBox9.TabIndex = 12;
+            // 
+            // textBox10
+            // 
+            this.textBox10.Location = new System.Drawing.Point(469, 110);
+            this.textBox10.Name = "textBox10";
+            this.textBox10.Size = new System.Drawing.Size(194, 20);
+            this.textBox10.TabIndex = 13;
+            // 
+            // textBox11
+            // 
+            this.textBox11.Location = new System.Drawing.Point(469, 149);
+            this.textBox11.Name = "textBox11";
+            this.textBox11.Size = new System.Drawing.Size(194, 20);
+            this.textBox11.TabIndex = 14;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(387, 34);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(55, 13);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "Süti neve:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(387, 72);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(61, 13);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "Süti típusa:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(387, 110);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(45, 13);
+            this.label6.TabIndex = 17;
+            this.label6.Text = "Egység:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(387, 152);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(20, 13);
+            this.label7.TabIndex = 18;
+            this.label7.Text = "Ár:";
+            // 
+            // chkBDij
+            // 
+            this.chkBDij.AutoSize = true;
+            this.chkBDij.Location = new System.Drawing.Point(390, 215);
+            this.chkBDij.Name = "chkBDij";
+            this.chkBDij.Size = new System.Drawing.Size(63, 17);
+            this.chkBDij.TabIndex = 19;
+            this.chkBDij.Text = "Díjazott";
+            this.chkBDij.UseVisualStyleBackColor = true;
+            // 
+            // btnMentes
+            // 
+            this.btnMentes.Location = new System.Drawing.Point(78, 322);
+            this.btnMentes.Name = "btnMentes";
+            this.btnMentes.Size = new System.Drawing.Size(163, 33);
+            this.btnMentes.TabIndex = 20;
+            this.btnMentes.Text = "Árajánlat mentése";
+            this.btnMentes.UseVisualStyleBackColor = true;
+            this.btnMentes.Click += new System.EventHandler(this.btnMentes_Click);
+            // 
+            // btnFelvetel
+            // 
+            this.btnFelvetel.Location = new System.Drawing.Point(456, 261);
+            this.btnFelvetel.Name = "btnFelvetel";
+            this.btnFelvetel.Size = new System.Drawing.Size(163, 33);
+            this.btnFelvetel.TabIndex = 21;
+            this.btnFelvetel.Text = "Új süti felvétele";
+            this.btnFelvetel.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(696, 367);
+            this.Controls.Add(this.btnFelvetel);
+            this.Controls.Add(this.btnMentes);
+            this.Controls.Add(this.chkBDij);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox11);
+            this.Controls.Add(this.textBox10);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.tbLegolcsobbAr);
+            this.Controls.Add(this.tbLegolcsobbNev);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.tbLegdragabbAr);
+            this.Controls.Add(this.tbLegdragabbNev);
+            this.Controls.Add(this.tbValaszthato);
+            this.Controls.Add(this.tbMaiAjanlat);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbMaiAjanlat;
+        private System.Windows.Forms.TextBox tbValaszthato;
+        private System.Windows.Forms.TextBox tbLegdragabbNev;
+        private System.Windows.Forms.TextBox tbLegdragabbAr;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox tbLegolcsobbNev;
+        private System.Windows.Forms.TextBox tbLegolcsobbAr;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.TextBox textBox10;
+        private System.Windows.Forms.TextBox textBox11;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.CheckBox chkBDij;
+        private System.Windows.Forms.Button btnMentes;
+        private System.Windows.Forms.Button btnFelvetel;
+    }
+}
+
---
+++ Cukraszda/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Cukraszda
+{
+    public partial class Form1 : Form
+    {
+        static List<Sutemeny> sutemenyek = new List<Sutemeny>();
+        public Form1()
+        {
+            InitializeComponent();
+            StreamReader be = new StreamReader("cuki.txt");
+            while (!be.EndOfStream)
+            {
+                string[] a = be.ReadLine().Split(';');
+                sutemenyek.Add(new Sutemeny(a[0], a[1], bool.Parse(a[2]), int.Parse(a[3]), a[4]));
+            }
+            be.Close();
+            Random();
+            MinMax();
+            DijazottDb();
+        }
+        public void Random()
+        {
+            Random rnd = new Random();
+            int rand = rnd.Next(0, sutemenyek.Count);
+            tbMaiAjanlat.Text = sutemenyek[rand].Nev;
+        }
+        public void MinMax()
+        {
+            foreach (var s in sutemenyek.OrderBy(x => x.Ar).Take(1))
+            {
+               tbLegolcsobbNev.Text = s.Nev;
+                tbLegolcsobbAr.Text = s.Ar +"/"+ s.Egyseg;
+            }
+            foreach (var s in sutemenyek.OrderByDescending(x => x.Ar).Take(1))
+            {
+                tbLegdragabbNev.Text = s.Nev;
+                tbLegdragabbAr.Text = s.Ar + "/" + s.Egyseg;
+            }
+        }
+        public void DijazottDb()
+        {
+            int db = 0;
+            foreach (var s in sutemenyek)
+            {
+                if (s.Dijazott)
+                {
+                    db++;
+                }
+            }
+            tbValaszthato.Text = db + "Féle díjnyertes édességből választhat";
+        }
+        public void ListaKiiras()
+        {
+
+        }
+
+        private void btnMentes_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
